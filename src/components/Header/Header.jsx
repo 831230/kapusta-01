5260f3b8d2119700cd1ec44e03961dedb4940235
Tomasz Bielecki <szkoleniegoit@gmail.com>
2024-05-08 21:21:27 +0200

    added routing

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import UserData from "../UserData/UserData";
+import ExitButton from "../ExitButton/ExitButton";
+
+const Header = () => {
+  return ( 
+    <header>
+      <div><img src="" alt="logo_kapusta" /></div>
+      <UserData/>
+      <ExitButton/>
+    </header>
+   );
+}
+ 
+export default Header;
